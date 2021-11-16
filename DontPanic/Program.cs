3bce9cb36413e711399d648fa96ab4f851c89719
Lisanne Nurk <lnurk@tktk.ee>
2021-11-16 11:50:08 +0200

    add new projects

---
+++ DontPanic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DontPanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm asendab kõik 'o' tähed lauses "Don't panic" nulliga - 0
+            //programm asendab kõik 'a' tähed samas lauses 4-ga
+
+            string dontP = "Don't panic!";
+
+            /* for(int i = 0; i < dontP.Length; i++)
+            {
+                dontP[i] = '*'; //sõnad on muutumatu
+            }*/
+
+            dontP = dontP.Replace('o', '0');
+            dontP = dontP.Replace('a', '4');
+            Console.WriteLine(dontP);
+
+        }
+    }
+}
---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kontrollib andmete pikkust
+            //programm kuvab kumb nendes on pikem, kas ees- või perekonnanime
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+
+            int firstNameLength = firstName.Length;
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit.");
+
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            int lastNameLength = lastName.Length;
+            Console.WriteLine($"Sine perekonnanimes on {lastName.Length} sümbolit.");
+
+            if(firstNameLength > lastNameLength)
+            {
+                Console.WriteLine("Sinu eesnimi on pikem kui perekonnanimi.");
+            }
+            if(lastNameLength > firstNameLength)
+            {
+                Console.WriteLine("Sinu perekonnanimi on pikem kui eesnimi.");
+            }
+            else if (firstNameLength == lastNameLength)
+            {
+                Console.WriteLine("Sinu ees- ja perekonnanimi on sama pikad.");
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
